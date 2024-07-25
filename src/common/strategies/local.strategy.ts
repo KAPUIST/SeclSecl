c7913540efe6bc00c5cd4108eb1e29eff0cdff51
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-07-26 00:56:04 +0900

    feat 에러메세지 수정 및 주석처리

--- src/common/strategies/local.strategy.ts
+++ src/common/strategies/local.strategy.ts
@@ -2,14 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
 import { Strategy } from 'passport-local'
 // import { AuthService } from '../../main/auth/auth.service'
-import { CpService } from '../../cp/cp.service'
+import { cpService } from '../../cp/cp.service'
 // import { AdminService } from '../../admin/auth/auth.service"
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(
     // private readonly authService: AuthService,
-    private readonly cpService: CpService,
+    private readonly cpService: cpService,
     // private readonly adminService: AdminService,
   ) {
     super({
@@ -21,15 +21,15 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, password: string) {
     const services = [
       // this.authService,
-      this.cpService,
+      // this.cpService,
       //  this.adminService
     ]
     for (const service of services) {
-      const user = await service.validateUser({ email, password })
-      if (user) {
-        return user
-      }
+      // const user = await service.validateUser({ email, password })
+      //   if (user) {
+      //     return user
+      //   }
     }
-    throw new UnauthorizedException('Invalid credentials')
+    throw new UnauthorizedException('일치하는 인증 정보가 없습니다.')
   }
 }
