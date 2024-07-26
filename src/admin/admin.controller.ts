d86d749e5174b3453b263db486c928e90384ceea
yewon kim <yewonkim@yewonui-MacBookPro.local>
2024-07-26 10:20:50 +0900

    update. cp 승인 기능

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,27 +1,41 @@
-import { Controller, Get, HttpStatus, Post } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger';
-import { get } from 'lodash';
-import { AdminService } from './admin.service';
+import { Controller, Get, HttpStatus, Param, Post } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { get } from 'lodash'
+import { AdminService } from './admin.service'
 
 @ApiTags('유저 승인')
 @Controller('cps')
 export class AdminController {
-    constructor(private readonly adminService: AdminService) {}
-/**
- * 가입 신청 리스트 조회
- * @returns 
- */
-    @Get('approval-list')
-    async getApprovalList(){
-        const data = await this.adminService.getApprovalList();
+  constructor(private readonly adminService: AdminService) {}
+  /**
+   * 가입 신청 리스트 조회
+   * @returns
+   */
+  @Get('signUp-cps')
+  async getApprovalList() {
+    const data = await this.adminService.getApprovalList()
 
-        return {
-            status: HttpStatus.OK,
-            message: '가입 신청 리스트 조회에 성공했습니다.',
-            data
-        }
+    return {
+      status: HttpStatus.OK,
+      message: '가입 신청 리스트 조회에 성공했습니다.',
+      data,
     }
+  }
 
-
+  /**
+   * cp 승인
+   * @param id 
+   * @returns 
+   */
+  @Post('siignup-cps/:cpId/approve')
+  async approveCp(@Param('id') id: string) {
+    const data = await this.adminService.approveCp(id);
+    
+    return {
+        status: HttpStatus.OK,
+        message: 'cp 승인에 성공했습니다.',
+        data
+    }
+  }
 
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,35 +1,48 @@
-import { Injectable } from '@nestjs/common'
-import { InjectRepository } from '@nestjs/typeorm';
-import { Cp } from 'src/cp/auth/entities/cp.entity';
-import { CpInfo } from '../cp/auth/entities/cp-infos.entity';
-import { Repository } from 'typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Cp } from 'src/cp/auth/entities/cp.entity'
+import { CpInfo } from '../cp/auth/entities/cp-infos.entity'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class AdminService {
-    constructor(
-        @InjectRepository(Cp, 'cp')
-        private readonly cpRepository: Repository<Cp>,
-        @InjectRepository(CpInfo, 'cp')
-        private readonly cpInfosRepository: Repository<CpInfo>,
-    ) {}
+  constructor(
+    @InjectRepository(Cp, 'cp')
+    private readonly cpRepository: Repository<Cp>,
+    @InjectRepository(CpInfo, 'cp')
+    private readonly cpInfosRepository: Repository<CpInfo>,
+  ) {}
 
-    //가입 신청 리스트 조회
-    async getApprovalList(): Promise<Partial<Cp>[]> {
+  //가입 신청 리스트 조회
+  async getApprovalList(): Promise<Partial<Cp>[]> {
     const cps = await this.cpRepository.find({
-        where: { isVerified: false },
-        relations: ['cpInfo']    
+      where: { isVerified: false },
+      relations: ['cpInfo'],
     })
-    return cps.map(cp => ({
-        uid: cp.uid,
-        email: cp.email,
-        isVerified: cp.isVerified,
-        createdAt: cp.createdAt,
-        cpInfos: {
-            name: cp.cpInfo.name,
-            description: cp.cpInfo.description,
-            phoneNumber: cp.cpInfo.phoneNumber,
-            address: cp.cpInfo.address,
-        },
-        }))
-      }
+    return cps.map((cp) => ({
+      uid: cp.uid,
+      email: cp.email,
+      isVerified: cp.isVerified,
+      createdAt: cp.createdAt,
+      cpInfos: {
+        name: cp.cpInfo.name,
+        description: cp.cpInfo.description,
+        phoneNumber: cp.cpInfo.phoneNumber,
+        address: cp.cpInfo.address,
+      },
+    }))
+  }
+
+  //cp 승인
+  async approveCp(id:string) {
+    const cp = await this.cpRepository.findOne({where: {uid:id}})
+    if(!cp) {
+        throw new NotFoundException('cp를 찾을 수 없습니다.');
     }
+    cp.isVerified = true
+    return this.cpRepository.save(cp)
+  }
+
+
+
+}
