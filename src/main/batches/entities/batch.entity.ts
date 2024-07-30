9aee073176203d2cfc34b485ab07116ee57a9e05
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-07-30 09:38:40 +0900

    fix/오타

--- src/main/batches/entities/batch.entity.ts
+++ src/main/batches/entities/batch.entity.ts
@@ -7,7 +7,7 @@ import {
   DeleteDateColumn,
   OneToMany,
 } from 'typeorm'
-import { UserLesson } from '../../users/entities/user-lessons..entity'
+import { UserLesson } from '../../users/entities/user-lessons.entity'
 
 @Entity({ name: 'batches' })
 export class Batch {
