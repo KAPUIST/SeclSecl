d9c1bc3a41044a0b5be54956b23837e8a28600bd
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-07-30 12:41:47 +0900

    fix/repo

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 REPOSITORY=/home/ubuntu/deploy-be # 배포된 프로젝트 경로.
 
-cd $REPOSITORY # 이 경로로 이동해서 밑에 명령어들을 차례로 실행.
+cd $REPOSITORY || exit # 이 경로로 이동 실패 시 스크립트 종료
+
+# NVM 및 Node.js 환경 설정
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # NVM 로드
+nvm use 20.15.1 # 사용할 Node.js 버전 설정
 
 # 기존 프로세스 종료
-/home/ubuntu/.nvm/versions/node/v20.15.1/bin/pm2 stop "seclsecl" && /home/ubuntu/.nvm/versions/node/v20.15.1/bin/pm2 delete "seclsecl"
+pm2 stop "seclsecl" && pm2 delete "seclsecl"
 
 # 새로 시작
-/home/ubuntu/.nvm/versions/node/v20.15.1/bin/pm2 start dist/main.js --name "seclsecl"
+pm2 start dist/main.js --name "seclsecl"
