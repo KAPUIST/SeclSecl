340334e7a2d04b5ed2da843fee7479f53511433c
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-07-30 12:38:07 +0900

    fix/test

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ REPOSITORY=/home/ubuntu/deploy-be # 배포된 프로젝트 경로.
 cd $REPOSITORY # 이 경로로 이동해서 밑에 명령어들을 차례로 실행.
 
 # 기존 프로세스 종료
-pm2 stop "seclsecl" && pm2 delete "seclsecl"
+/home/ubuntu/.nvm/versions/node/v20.15.1/bin/pm2 stop "seclsecl" && /home/ubuntu/.nvm/versions/node/v20.15.1/bin/pm2 delete "seclsecl"
 
-pm2 start dist/main.js --name "seclsecl"
+# 새로 시작
+/home/ubuntu/.nvm/versions/node/v20.15.1/bin/pm2 start dist/main.js --name "seclsecl"
