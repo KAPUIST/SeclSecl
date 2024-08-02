3cb143efe24a8a65384f1b5c1065416f49c1fcd7
mmomomoo <164153608+mmomomoo@users.noreply.github.com>
2024-08-02 10:55:48 +0900

    fix/ 파일 2개 생성된 부분 수정

--- src/main/payments/dto/delete-cart-params.dto 2.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsUUID } from 'class-validator'
-import { MAIN_MESSAGE_CONSTANT } from '../../../common/messages/main.message'
-
-export class DeleteCartParamsDTO {
-  @IsUUID()
-  @IsNotEmpty({ message: MAIN_MESSAGE_CONSTANT.PAYMENT.COMMON.PAYMENT_CART_ENTITY.CART_UID })
-  cartUid: string
-}
