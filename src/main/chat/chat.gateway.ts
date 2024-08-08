eccddea8921058d1619653a8d8fce52cdf54b493
yewon kim <yewonkim@yewonui-MacBookPro.local>
2024-08-08 23:31:53 +0900

    소켓 네임스페이스 추가

--- src/main/chat/chat.gateway.ts
+++ src/main/chat/chat.gateway.ts
@@ -14,6 +14,7 @@ import { ChatService } from './chat.service'
     exposedHeaders: ['Authorization'],
     credentials: true,
   },
+  namespace:'chatting'
 })
 // @UseGuards(WsAuthGuard)
 export class ChatGateway {
