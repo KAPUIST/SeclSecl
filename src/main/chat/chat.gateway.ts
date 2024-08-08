d6796e4687c509f0ec5b88a380bf615c7c09fbbf
yewon kim <yewonkim@yewonui-MacBookPro.local>
2024-08-08 17:54:10 +0900

    fix. 소켓 연결 경로 수정 테스트

--- src/main/chat/chat.gateway.ts
+++ src/main/chat/chat.gateway.ts
@@ -14,6 +14,7 @@ import { ChatService } from './chat.service'
     credentials: true,
   },
   namespace: 'chatting',
+  path: '/socket.io'
 })
 // @UseGuards(WsAuthGuard)
 export class ChatGateway {
