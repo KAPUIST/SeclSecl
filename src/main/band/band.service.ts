3721ead9aa039aff4101a38dac3beae9522ee91c
Budubuu <162087289+Budubuu@users.noreply.github.com>
2024-08-12 14:30:40 +0900

    fix/joinband 로직변경

--- src/main/band/band.service.ts
+++ src/main/band/band.service.ts
@@ -204,11 +204,12 @@ export class BandService {
     if (_.isNil(band)) {
       throw new NotFoundException(MAIN_MESSAGE_CONSTANT.BAND.BAND_GROUP.JOIN_BAND.NOT_FOUND)
     }
-    // 이미 가입했을 시 에러 처리
+    // 이미 가입했을 시 null 반환
     const isJoined = await this.bandMemberRepository.findOne({ where: { bandUid, userUid } })
     if (isJoined) {
-      throw new ConflictException('이미 가입된 유저입니다.')
+      throw null
     }
+    //가입되지 않은 경우 가입 처리
     await this.bandMemberRepository.save({ userUid, bandUid })
     return {
       userUid,
