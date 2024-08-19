8674cf99a2fa71daca12ff6c043ef914ad7ae6f3
Budubuu <162087289+Budubuu@users.noreply.github.com>
2024-08-19 12:23:26 +0900

    fix/기수 조회 모집기간 필터링 추가

--- src/main/chat/chat.service.ts
+++ src/main/chat/chat.service.ts
@@ -118,7 +118,6 @@ export class ChatService {
     for (const chatRoom of chatRooms) {
       const lastMessage = chatRoom.messages.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())[0]
 
-
       //내가 안 읽은 메세지 있는지 확인
       const unreadMessagesExist = chatRoom.messages.some((message) => message.sender !== uid && !message.isRead)
 
